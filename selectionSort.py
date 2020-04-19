75e0f3297a3c9bdd71be31088e12548eb61e63cd
Andres Campuzano <campuxano88@gmail.com>
2020-04-19 18:37:08 -0500

    selectionSort.py

--- selectionSort.py
+++ selectionSort.py
@@ -1,6 +1,8 @@
 # Buscar el numero menor en mi array.
 # Crear dos subarrays para llevar el control de mi algoritmo.
 # Imprimir el resultado del ordenamiento.
+import sys
+array = [20, 5, 21, 6, 23, 7, 34, 999, 68]
 
 
 def selectionSort(array):
@@ -12,6 +14,13 @@ def selectionSort(array):
             if array[idxDes] > array[j]:
                 idxDes = j
 
-         # ya que encontramos el minimo elemento,
-         # lo vamos a cambiar por el primer valor de nuestro array desordenado.
-         array[i],array[idxDes] = array[idxDes], array[i]
+        # ya que encontramos el minimo elemento,
+        # lo vamos a cambiar por el primer valor de nuestro array desordenado.
+        array[i], array[idxDes] = array[idxDes], array[i]
+
+
+# ejecutar la funcion
+selectionSort(array)
+print("Array ordenado:")
+for i in range(len(array)):
+    print("%d" % array[i]),
